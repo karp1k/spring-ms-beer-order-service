97870a52df43f397fe2466ce3abba269631b5679
Tom <desktop-vm-ubuntu>
2020-05-17 11:16:29 +0300

    Close #9 Add transitions for NEW to VALIDATION states Fix misspelling in configuration

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
@@ -3,7 +3,7 @@ package guru.sfg.beer.order.service.domain;
 /**
  * @author kas
  */
-public enum BeerOrderEvents {
+public enum BeerOrderEventEnum {
     VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED,
     ALLOCATION_SUCCESS, ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED,
     BEER_ORDER_PICKED_UP
